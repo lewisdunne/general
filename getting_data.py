099e848ac8e68d5ef0d5814b6e97755db7be36f7
Lewis Dunne <44022545+lewisdunne@users.noreply.github.com>
2019-05-26 19:14:10 +0100

    Create getting_data.py

---
+++ getting_data.py
@@ -0,0 +1,21 @@
+'''GETTING DATA FILES FROM A DIRECTORY'''
+import os # Use the os library to identify files.
+
+#-Function -------------------------------------------------------------------#
+def read_files(path, extension=None):
+    '''Reads all files into a list if they have a given extension.
+    If extension is undefined, just returns all files. '''
+    if extension == None:
+        files = os.listdir(path)
+    else:
+        files = [f for f in os.listdir(path) if f.split('.')[-1] == extension]
+    
+    return files
+    
+#-Example run ----------------------------------------------------------------#
+# Define the path in which your datafiles exist.
+path = 'C://Users//L//Documents//PsychoPy//MemANT//data'
+
+files = read_files(path, extension='csv')
+
+print(files)
