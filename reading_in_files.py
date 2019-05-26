687490042a0506b8e27b79ac60867e85d4de3058
Lewis Dunne <44022545+lewisdunne@users.noreply.github.com>
2019-05-26 19:23:48 +0100

    Create reading_in_files.py
    Example of how to read in files and aggregate them.

---
+++ reading_in_files.py
@@ -0,0 +1,31 @@
+'''GETTING DATA FILES FROM A DIRECTORY'''
+import os # Use the os library to identify files.
+
+#-Function -------------------------------------------------------------------#
+def get_files(path, extension=None):
+    '''Reads all files into a list if they have a given extension.
+    If extension is undefined, just returns all files. '''
+    if extension == None:
+        files = os.listdir(path)
+    else:
+        files = [f for f in os.listdir(path) if f.split('.')[-1] == extension]
+    
+    return files
+    
+#-Example run ----------------------------------------------------------------#
+# Define the path in which your datafiles exist.
+path = 'C://Users//L//Documents//PsychoPy//MemANT//data'
+
+files = read_files(path, extension='csv')
+
+print(files)
+
+#-Example using pandas to aggregate all data files into one.
+import pandas as pd # Use pandas for working with dataframes
+# Loop over the files and read them in
+alldata = [] # Empty list, will become a list of dataframes that we unify
+for f in files:
+    alldata.append(pd.read_csv(f))
+
+df = pd.concat(alldata)
+print(df.head()) # Print out the first 5 rows.
